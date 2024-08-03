246cf0f4b89b73a39c7de839804ae118006a0d72
KINGSHUK SADHU <72314053+Kingshuk99@users.noreply.github.com>
2024-08-03 23:26:28 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,7 @@ exports.getUserById = async(req, res) => {    //userid aftr authorization
         res.json(user);
     }
     catch(err) {
-        res.status(500).json({message:"Failed to fetch user",error:err.message});
+        res.status(500).json({message:"Failed to fetch user",error:err.message, req:req});
     }
 }
 
